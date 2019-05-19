39e345327192bcafe9dc8d25f4dffb3c5ded8c92
Arlan Uy <gnhs.com@gmail.com>
2019-05-19 19:15:41 +0800

    Name edit

--- Second_order_difference_formula.py
+++ Second_order_difference_formula.py
@@ -1,7 +1,25 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# In[42]:
+'''
+
+Arlan Vincent Uy
+
+2015-09385
+
+CS 131 THU
+
+'''
+
+
+
+'''
+
+Juico, Jules Gerard E.
+
+2014-40314
+
+CS 131 THU
+
+'''
 
 
 def e_func(x):
@@ -16,23 +34,22 @@ def findTripleFDiff(h, find_at):
 
 # In[45]:
 
+def main:
+	import math
 
-import math
-
-find_at = 0
-h_values = [10**(x-10) for x in range(0, 10)]
-#print(h_values)
-x = 0.1
-
-
-# In[46]:
+	find_at = 0
+	h_values = [10**(x-10) for x in range(0, 10)]
+	#print(h_values)
+	x = 0.1
 
 
-for h in h_values:
-    print("at h = " + str(h) + "the estimated value of f at x = " + str(find_at) + " is " + str(findTripleFDiff(h, find_at)))
 
 
-# In[ ]:
+
+	for h in h_values:
+	    print("at h = " + str(h) + "the estimated value of f at x = " + str(find_at) + " is " + str(findTripleFDiff(h, find_at)))
+
+main()
 
 
 
